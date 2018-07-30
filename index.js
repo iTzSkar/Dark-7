c5c0f824cc43515f2a14db7545868e3d62392bcb
iTzSkar <38841221+iTzSkar@users.noreply.github.com>
2018-07-30 12:43:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+const prefix = 'برفكس'
 
-console.log("Welcome Savage.");
- 
-client.on('message', message => {
-  if(message.content.startsWith(prefix + "join")) {
-  message.member.voiceChannel.join();
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
   }
-  });
+});
+
+
+client.on('message', message => {
+  if (message.content.startsWith('play')) {
+    const voiceChannel = message.member.voiceChannel;
+    voiceChannel.join()
+      });
+  } else if(message.content.startsWith(prefix + "join")) {
+message.member.voiceChannel.join();
+}
+});
+
   
 
 client.login(process.env.BOT_TOKEN); 
